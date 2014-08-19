d74f8aa1a981859701b36cdf79334d5e483d1063
Karthik Keyan Balan <karthikkeyan.b@ymedialabs.com>
2014-08-19 10:44:05 -0700

    [OR-1] Consent : Prototype for consonent scrollview.

---
+++ Consent/Consent/ConsentTutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConsentTutorialViewController.h
+//  Consent
+//
+//  Created by Karthik Keyan on 8/18/14.
+//  Copyright (c) 2014 Karthik Keyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsentTutorialViewController : UIViewController
+
+@end
---
+++ Consent/Consent/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Consent
+//
+//  Created by Karthik Keyan on 8/18/14.
+//  Copyright (c) 2014 Karthik Keyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
