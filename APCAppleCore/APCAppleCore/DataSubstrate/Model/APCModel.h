438e4d85efab42b138a00003eec5e756054b9650
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-08-28 13:13:08 -0700

    Added APC Models

---
+++ APCAppleCore/APCAppleCore/DataSubstrate/Model/APCModel.h
@@ -0,0 +1,13 @@
+//
+//  APCModel.h
+//  APCAppleCore
+//
+//  Created by Dhanush Balachandran on 8/28/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#ifndef APCAppleCore_APCModel_h
+#define APCAppleCore_APCModel_h
+
+
+#endif
