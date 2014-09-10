e39d12db5146a78fe79020a5bbfe799c7f8ff7ec
Justin Warmkessel <justin.w@ymedialabs.com>
2014-09-09 17:08:47 -0700

    Added APCParameters and related files

---
+++ APCAppleCore/APCAppleCore/Parameters/APCParameters.h
@@ -0,0 +1,25 @@
+//
+//  Parameters.h
+//  Parameters
+//
+//  Created by Karthik Keyan on 8/14/14.
+//  Copyright (c) 2014 Karthik Keyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const ParametersValueChangeNotification;
+
+
+@interface APCParameters : NSObject
+
+// Use [NSObject valueForKey:] to get value and [NSObject setValue:forKey:] to set values
+- (NSArray *) allKeys;
+
+// Clear all value and load values freshly from bundle
+- (void) reset;
+
+// Writes all changes into file
+- (BOOL) save;
+
+@end
