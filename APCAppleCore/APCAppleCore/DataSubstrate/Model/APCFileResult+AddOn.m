42e46ef40a01dc398940a99870c87d170c8b35e7
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-09-24 16:19:02 -0700

    Removed NSLog in APCFileResult

--- APCAppleCore/APCAppleCore/DataSubstrate/Model/APCFileResult+AddOn.m
+++ APCAppleCore/APCAppleCore/DataSubstrate/Model/APCFileResult+AddOn.m
@@ -34,7 +34,6 @@
     APCFileResult * localAPCResult = (APCFileResult*) apcResult;
 
     NSData * data = [NSData dataWithContentsOfFile:localRKResult.fileUrl.path];
-    NSLog(@"LENGTH: %ld Read File: %@", (unsigned long)[data length], data);
     localAPCResult.file = data;
     
 }
