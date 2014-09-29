0e53667aaee51543f06bbb06fd30d10010277755
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-09-29 09:46:25 -0700

    Updated Copyright Information

--- APCAppleCore/APCAppleCore/Library/Categories/HKHealthStore+APCExtensions.m
+++ APCAppleCore/APCAppleCore/Library/Categories/HKHealthStore+APCExtensions.m
@@ -1,12 +1,10 @@
-/*
- Copyright (C) 2014 Apple Inc. All Rights Reserved.
- See LICENSE.txt for this sample’s licensing information
- 
- Abstract:
- 
- Contains shared helper methods on HKHealthStore that are specific to Fit's use cases.
- 
- */
+//
+//  HKHealthStore+APCExtensions.m
+//  APCAppleCore
+//
+//  Created by Dhanush Balachandran on 9/26/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
 
 #import "HKHealthStore+APCExtensions.h"
 
