59e5791e62da9bbf06016f3379a5dacc0f43a7d2
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-10-03 09:37:03 -0700

    Added temporary email verify VC

---
+++ APCAppleCore/APCAppleCore/UI/Onboarding/TempUI/APCEmailVerifyViewController.m
@@ -0,0 +1,23 @@
+//
+//  APCEmailVerifyViewController.m
+//  APCAppleCore
+//
+//  Created by Dhanush Balachandran on 10/3/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import "APCEmailVerifyViewController.h"
+
+@interface APCEmailVerifyViewController ()
+
+@end
+
+@implementation APCEmailVerifyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
