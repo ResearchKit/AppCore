b8a960fbceb2aea2a9e6c451bf26ffce9f5cd541
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-11-02 22:40:09 -0800

    Add Present Animator

---
+++ APCAppleCore/APCAppleCore/UI/GraphCharts/APCPresentAnimator.h
@@ -0,0 +1,13 @@
+//
+//  APCPresentAnimator.h
+//  APCAppleCore
+//
+//  Created by Ramsundar Shandilya on 11/2/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APCPresentAnimator : NSObject
+
+@end
---
+++ APCAppleCore/APCAppleCore/UI/GraphCharts/APCPresentAnimator.m
@@ -0,0 +1,13 @@
+//
+//  APCPresentAnimator.m
+//  APCAppleCore
+//
+//  Created by Ramsundar Shandilya on 11/2/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import "APCPresentAnimator.h"
+
+@implementation APCPresentAnimator
+
+@end
