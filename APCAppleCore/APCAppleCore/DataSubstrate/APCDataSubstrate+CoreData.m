24b5a4cd45c056e9213c4e45185ce4a57d766d50
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-11-04 16:13:15 -0800

    Added Counts for scheduled tasks

--- APCAppleCore/APCAppleCore/DataSubstrate/APCDataSubstrate+CoreData.m
+++ APCAppleCore/APCAppleCore/DataSubstrate/APCDataSubstrate+CoreData.m
@@ -123,4 +123,19 @@
     return [self.mainContext executeFetchRequest:request error:&error];
 }
 
+- (NSUInteger)allScheduledTasksForToday
+{
+    NSFetchRequest * request = [APCScheduledTask request];
+    NSError* error;
+    return [self.mainContext countForFetchRequest:request error:&error];
+}
+
+- (NSUInteger)completedScheduledTasksForToday
+{
+    NSFetchRequest * request = [APCScheduledTask request];
+    request.predicate = [NSPredicate predicateWithFormat:@"completed == %@", @YES];
+    NSError* error;
+    return [self.mainContext countForFetchRequest:request error:&error];
+}
+
 @end
