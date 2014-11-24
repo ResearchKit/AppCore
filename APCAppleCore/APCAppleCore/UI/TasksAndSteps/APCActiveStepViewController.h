301b2ea76e1112e5b879a4fdb772176369136334
Henry McGilton <henry.mcgilton@ymedialabs.com>
2014-11-23 22:19:38 -0800

    Re-instating  APCActiveStepViewController  which got deleted during a merge.

---
+++ APCAppleCore/APCAppleCore/UI/TasksAndSteps/APCActiveStepViewController.h
@@ -0,0 +1,14 @@
+//
+//  APCActiveStepViewController.h
+//  Parkinson
+//
+//  Created by Henry McGilton on 11/20/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import <ResearchKit/ResearchKit.h>
+#import <ResearchKit/ResearchKit_Private.h>
+
+@interface APCActiveStepViewController : RKSTActiveStepViewController
+
+@end
---
+++ APCAppleCore/APCAppleCore/UI/TasksAndSteps/APCActiveStepViewController.m
@@ -0,0 +1,31 @@
+//
+//  APCActiveStepViewController.m
+//  Parkinson
+//
+//  Created by Henry McGilton on 11/20/14.
+//  Copyright (c) 2014 Y Media Labs. All rights reserved.
+//
+
+#import "APCActiveStepViewController.h"
+#import "APCAppleCore.h"
+
+@interface APCActiveStepViewController ()
+
+@end
+
+@implementation APCActiveStepViewController
+
+    //
+    //    over-ride for stepDidFinish
+    //
+
+- (void)stepDidFinish
+{
+    if (self.delegate != nil) {
+        if ([self.delegate respondsToSelector:@selector(stepViewControllerDidFinish:navigationDirection:)] == YES) {
+            [self.delegate stepViewControllerDidFinish:self navigationDirection:RKSTStepViewControllerNavigationDirectionForward];
+        }
+    }
+}
+
+@end
