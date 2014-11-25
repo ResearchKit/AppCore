2228800d6582ddfbc6a26163e2f9393fad63aae1
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2014-11-24 23:47:45 -0800

    Add Pie Chart Component

---
+++ APCAppleCore/APCAppleCore/UI/Components/APCPieGraphView.h
@@ -0,0 +1,55 @@
+//
+//  APCPieGraphView.h
+//  PieChart
+//
+//  Created by Ramsundar Shandilya on 11/24/14.
+//  Copyright (c) 2014 Ramsundar Shandilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol APCPieGraphViewDatasource;
+
+@interface APCPieGraphView : UIView
+
+@property (nonatomic) CGFloat pieGraphRadius;
+
+@property (nonatomic) CGFloat legendDotRadius;
+
+@property (nonatomic) CGFloat legendPaddingHeight;
+
+@property (nonatomic) CGFloat lineWidth;
+
+@property (nonatomic) BOOL shouldAnimate;
+
+@property (nonatomic) CGFloat animationDuration;
+
+@property (nonatomic, strong) UIFont *legendFont;
+
+@property (nonatomic, strong) UIFont *percentageFont;
+
+@property (nonatomic, weak) id <APCPieGraphViewDatasource> datasource;
+
+@property (nonatomic) BOOL hidesPercentageLabels;
+
+@property (nonatomic) BOOL hidesLegend;
+
+@property (nonatomic) BOOL hidesCenterLabels;
+
+@end
+
+@protocol APCPieGraphViewDatasource <NSObject>
+
+@required
+
+- (NSInteger)numberOfSegmentsInPieGraphView;
+
+- (CGFloat)pieGraphView:(APCPieGraphView *)pieGraphView valueForSegmentAtIndex:(NSInteger)index;
+
+@optional
+
+- (UIColor *)pieGraphView:(APCPieGraphView *)pieGraphView colorForSegmentAtIndex:(NSInteger)index;
+
+- (NSString *)pieGraphView:(APCPieGraphView *)pieGraphView titleForSegmentAtIndex:(NSInteger)index;
+
+@end
