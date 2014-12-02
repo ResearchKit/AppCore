b66c56d126db748f41d3e960f2c8131a0b34e8eb
Edward Cessna <edward.cessna@ymedialabs.com>
2014-12-02 14:06:12 -0800

    [APPLE-527]Script checks for git repository
    [APPLE-527]The script checks for a git repository and, if it does not find one, echos a message to the effect and does NOT set the version or build number.

--- APCAppleCore/APCAppleCore/Resources/update_version.sh
+++ APCAppleCore/APCAppleCore/Resources/update_version.sh
@@ -8,11 +8,17 @@
 set -o nounset	# Script exits if an undeclared variable is used
 set -o errexit	# Script exits if a command fails
 
-VERSION=`git describe --tags --always --dirty`
-BUILD=`git rev-list HEAD | wc -l | tr -d ' '`
-
-echo "     Updating version to: ${VERSION}"
-echo "Updating build number to: ${BUILD}"
-
-defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleShortVersionString" "${VERSION}"
-defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleVersion" "${BUILD}"
+if git symbolic-ref HEAD 2>/dev/null	#	Are we in a git repository?
+then
+	VERSION=`git describe --tags --always --dirty`
+	BUILD=`git rev-list HEAD | wc -l | tr -d ' '`
+	
+	echo "     Updating version to: ${VERSION}"
+	echo "Updating build number to: ${BUILD}"
+	
+	defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleShortVersionString" "${VERSION}"
+	defaults write "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH%.*}" "CFBundleVersion" "${BUILD}"
+else
+	echo "git repository was not detected"
+	echo "Version and build numbers were not automatically set."
+fi
