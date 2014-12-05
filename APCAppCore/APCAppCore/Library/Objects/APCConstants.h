2c5bdd985320f3751791cfd50cf1c586c049761b
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-12-05 11:38:22 -0800

    Added support for multiple HKObjectType

--- APCAppCore/APCAppCore/Library/Objects/APCConstants.h
+++ APCAppCore/APCAppCore/Library/Objects/APCConstants.h
@@ -45,6 +45,12 @@ FOUNDATION_EXPORT NSString *const kHKReadPermissionsKey;
 FOUNDATION_EXPORT NSString *const kAppServicesListRequiredKey;
 FOUNDATION_EXPORT NSString *const kVideoURLKey;
 
+FOUNDATION_EXPORT NSString *const kHKQuantityTypeKey;
+FOUNDATION_EXPORT NSString *const kHKCategoryTypeKey;
+FOUNDATION_EXPORT NSString *const kHKCharacteristicTypeKey;
+FOUNDATION_EXPORT NSString *const kHKCorrelationTypeKey;
+FOUNDATION_EXPORT NSString *const kHKWorkoutTypeKey;
+
 FOUNDATION_EXPORT NSString *const kPasswordKey;
 FOUNDATION_EXPORT NSString *const kSessionTokenKey;
 FOUNDATION_EXPORT NSString *const kNumberOfMinutesForPasscodeKey;
