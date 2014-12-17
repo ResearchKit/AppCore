9c80719db4a011a7a0c8d12057766b1694c343c4
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2014-12-17 10:05:35 -0800

    Adding an archive method for use by the diabetes glucose log.

--- APCAppCore/APCAppCore/Library/Categories/RKSTTaskResult+Archiver.m
+++ APCAppCore/APCAppCore/Library/Categories/RKSTTaskResult+Archiver.m
@@ -62,6 +62,56 @@ NSString *const kCertFileName = @"rsacert";
     return retValue;
 }
 
+- (NSString *)archiveResults:(NSArray *)results WithFilePath:(NSString *)filePath
+{
+    //Archive
+    RKSTDataArchive * archive = [[RKSTDataArchive alloc] initWithItemIdentifier:self.identifier
+                                                                studyIdentifier:((APCAppDelegate*)[UIApplication sharedApplication].delegate).defaultInitializationOptions[kStudyIdentifierKey]
+                                                                    taskRunUUID:self.taskRunUUID
+                                                                  extraMetadata:nil
+                                                                 fileProtection:RKFileProtectionCompleteUnlessOpen];
+    
+    [results enumerateObjectsUsingBlock:^(RKSTStepResult *stepResult, NSUInteger idx, BOOL *stop) {
+        [stepResult.results enumerateObjectsUsingBlock:^(RKSTResult *result, NSUInteger idx, BOOL *stop) {
+            //Update date if needed
+            if (!result.startDate) {
+                result.startDate = stepResult.startDate;
+                result.endDate = stepResult.endDate;
+            }
+            
+            if ([result isKindOfClass:[RKSTDataResult class]])
+            {
+                //TODO: Figure out contentType & metaData
+                RKSTDataResult * dataResult = (RKSTDataResult*) result;
+                NSError * archiveError;
+                [archive addContentWithData:dataResult.data filename:[dataResult.identifier stringByAppendingString:@"_data"] contentType:@"data" timestamp:dataResult.endDate metadata:nil error:&archiveError];
+                APCLogError2 (archiveError);
+            }
+            else if ([result isKindOfClass:[RKSTFileResult class]])
+            {
+                //TODO: Figure out contentType & metaData
+                RKSTFileResult * fileResult = (RKSTFileResult*) result;
+                NSError * archiveError;
+                [archive addFileWithURL:fileResult.fileUrl contentType:@"data" metadata:nil error:&archiveError];
+                APCLogError2 (archiveError);
+            }
+            else
+            {
+                NSError * archiveError;
+                [result addToArchive:archive error:&archiveError];
+                APCLogError2 (archiveError);
+            }
+        }];
+    }];
+    
+    NSError * archiveError;
+    NSData * certFile = [self readPEM];
+    NSData * data = (certFile) ? [archive archiveDataEncryptedWithIdentity:certFile error:&archiveError] : [archive archiveDataWithError:&archiveError];
+    NSString * fileName = (certFile) ? @"encrypted.zip" : @"unencrypted.zip";
+    NSString * retValue = [self writeData:data toFileName:fileName inPath:filePath] ? fileName : nil;
+    return retValue;
+}
+
 - (BOOL) writeData: (NSData*) data toFileName: (NSString*) fileName inPath: (NSString*) filePath
 {
     if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
