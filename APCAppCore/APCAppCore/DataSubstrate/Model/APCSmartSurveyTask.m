973370bd43f2e3b8ffd45f2ad9864591c0f59693
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-12-18 21:32:28 -0800

    Conditional Surveys finished

--- APCAppCore/APCAppCore/DataSubstrate/Model/APCSmartSurveyTask.m
+++ APCAppCore/APCAppCore/DataSubstrate/Model/APCSmartSurveyTask.m
@@ -10,6 +10,17 @@
 #import <BridgeSDK/BridgeSDK.h>
 #import "APCAppCore.h"
 
+NSString *const kRuleOperatorKey = @"operator";
+NSString *const kRuleSkipToKey = @"skipTo";
+NSString *const kRuleValueKey = @"value";
+
+NSString *const kOperatorSkip               = @"de";
+NSString *const kOperatorEqual              = @"eq";
+NSString *const kOperatorNotEqual           = @"ne";
+NSString *const kOperatorLessThan           = @"lt";
+NSString *const kOperatorGreaterThan        = @"gt";
+NSString *const kOperatorLessThanEqual      = @"le";
+NSString *const kOperatorGreaterThanEqual   = @"ge";
 
 
 @class APCDummyObject;
@@ -61,7 +72,7 @@ static APCDummyObject * _dummyObject;
             
             NSArray * rulesArray = [[obj constraints] rules];
             if (rulesArray) {
-                self.rules[obj.identifier] = rulesArray;
+                self.rules[obj.identifier] = [self createArrayOfDictionaryForRules:rulesArray];
             }
 
         }];
@@ -74,51 +85,53 @@ static APCDummyObject * _dummyObject;
     return self;
 }
 
-- (NSString *) nextStepIdentifier: (BOOL) after currentIdentifier: (NSString*) currentIdentifier
+- (NSArray*) createArrayOfDictionaryForRules: (NSArray*) rulesArray
 {
-    if (currentIdentifier == nil && after) {
-        return self.staticStepIdentifiers[0];
-    }
-    NSInteger currentIndex = [self.staticStepIdentifiers indexOfObject: currentIdentifier];
-    NSAssert(currentIndex != NSNotFound, @"Step Not Found. Should not get here.");
-    NSInteger newIndex = NSNotFound;
-    if (after) {
-        if (currentIndex+1 < self.staticStepIdentifiers.count) {
-            newIndex = currentIndex + 1;
+    NSMutableArray * newRulesArray = [NSMutableArray array];
+    [rulesArray enumerateObjectsUsingBlock:^(SBBSurveyRule * rule, NSUInteger idx, BOOL *stop) {
+        NSMutableDictionary * ruleDict = [NSMutableDictionary dictionary];
+        if (rule.operator) {
+            ruleDict[kRuleOperatorKey] = rule.operator;
         }
-    }
-    else
-    {
-        if ((currentIndex - 1) >= 0) {
-            newIndex = currentIndex -1;
+        if (rule.value) {
+            ruleDict[kRuleValueKey] = rule.value;
         }
-    }
-    return (newIndex != NSNotFound) ? self.staticStepIdentifiers[newIndex] : nil;
+        if (rule.skipTo) {
+            ruleDict[kRuleSkipToKey] = rule.skipTo;
+        }
+        
+        [newRulesArray addObject:ruleDict];
+    }];
+    return newRulesArray;
 }
+
 
 - (RKSTStep *)stepAfterStep:(RKSTStep *)step withResult:(RKSTTaskResult *)result
 {
-    APCLogDebug(@"AFTER     :  Step Identifier: %@", step.identifier);
-    if (step.identifier && ![self.currentStep isEqualToString:step.identifier]) {
-        self.currentStep = step.identifier;
-        //STEP 1: Refill dynamic Array from current step forward
-        [self refillDynamicStepIdentifiersWithCurrentStepIdentifier:step.identifier];
-        
-        //STEP 2: Remove unnecessary steps
-//        RKSTResult * stepResult = [result resultForIdentifier:step.identifier];
-//        APCLogDebug(@"Step Identifier: %@ StepResult: %@", step.identifier, stepResult);
-    }
-
+    [self refillDynamicStepIdentifiersWithCurrentStepIdentifier:step.identifier];
     
-    //STEP 3:
+    //If Step has rules, process answer. Otherwise keep moving
+    NSArray * rulesForThisStep = self.rules[step.identifier];
+    NSString * skipToStep = nil;
+    if (rulesForThisStep.count) {
+        RKSTStepResult * stepResult = (RKSTStepResult*) [result resultForIdentifier:step.identifier];
+        id firstResult = stepResult.results.firstObject;
+        if (firstResult == nil || [firstResult isKindOfClass:[RKSTQuestionResult class]]) {
+            skipToStep = [self processRules:rulesForThisStep forAnswer:[firstResult answer]];
+        }
+        //If there is new skipToStep then skip to that step
+        if (skipToStep) {
+            [self adjustDynamicStepIdentifersForSkipToStep:skipToStep from:step.identifier];
+        }
+    }
+    
     NSString * nextStepIdentifier = [self nextStepIdentifier:YES currentIdentifier:step.identifier];
     return nextStepIdentifier? self.rkSteps[nextStepIdentifier] : nil;
 }
 
 - (RKSTStep *)stepBeforeStep:(RKSTStep *)step withResult:(RKSTTaskResult *)result
 {
-    APCLogDebug(@"BEFORE    : Step Identifier: %@", step.identifier);
-    self.currentStep = step.identifier;
+    [self refillDynamicStepIdentifiersWithCurrentStepIdentifier:step.identifier];
     NSString * nextStepIdentifier = [self nextStepIdentifier:NO currentIdentifier:step.identifier];
     return nextStepIdentifier? self.rkSteps[nextStepIdentifier] : nil;
 }
@@ -128,6 +141,44 @@ static APCDummyObject * _dummyObject;
     return RKSTTaskProgressMake([self.staticStepIdentifiers indexOfObject: step.identifier] + 1, self.staticStepIdentifiers.count);
 }
 
+/*********************************************************************************/
+#pragma mark - Array Processing
+/*********************************************************************************/
+- (NSString *) nextStepIdentifier: (BOOL) after currentIdentifier: (NSString*) currentIdentifier
+{
+    if (currentIdentifier == nil && after) {
+        return self.dynamicStepIdentifiers[0];
+    }
+    NSInteger currentIndex = [self.dynamicStepIdentifiers indexOfObject: currentIdentifier];
+    NSAssert(currentIndex != NSNotFound, @"Step Not Found. Should not get here.");
+    NSInteger newIndex = NSNotFound;
+    if (after) {
+        if (currentIndex+1 < self.dynamicStepIdentifiers.count) {
+            newIndex = currentIndex + 1;
+        }
+    }
+    else
+    {
+        if ((currentIndex - 1) >= 0) {
+            newIndex = currentIndex -1;
+        }
+    }
+    return (newIndex != NSNotFound) ? self.dynamicStepIdentifiers[newIndex] : nil;
+}
+
+- (void) adjustDynamicStepIdentifersForSkipToStep: (NSString*) skipToStep from: (NSString*) currentStep
+{
+    NSInteger currentIndex = [self.dynamicStepIdentifiers indexOfObject:currentStep];
+    NSInteger skipToIndex = [self.dynamicStepIdentifiers indexOfObject:skipToStep];
+    NSAssert(currentIndex != NSNotFound, @"Should not happen");
+    NSAssert(skipToIndex != NSNotFound, @"Should not happen");
+    
+    if (skipToIndex > currentIndex) {
+        while (![self.dynamicStepIdentifiers[currentIndex+1] isEqualToString:skipToStep]) {
+            [self.dynamicStepIdentifiers removeObjectAtIndex:currentIndex+1];
+        }
+    }
+}
 
 - (void) refillDynamicStepIdentifiersWithCurrentStepIdentifier: (NSString*) stepIdentifier
 {
@@ -145,6 +196,151 @@ static APCDummyObject * _dummyObject;
     NSArray * subArray = [self.staticStepIdentifiers objectsAtIndexes:indexSet];
     
     [self.dynamicStepIdentifiers addObjectsFromArray:subArray];
+}
+
+
+
+/*********************************************************************************/
+#pragma mark - Rule Checking
+/*********************************************************************************/
+
+- (NSString *) processRules: (NSArray*) rules forAnswer: (id) answer
+{
+    /**
+     * Check if answer is nil (Skip) or NSNumber or NSString then process rules. Otherwise no processing of rules.
+     *      Single choice: the selected RKAnswerOption's `value` property. SUPPORTED
+     *      Multiple choice: array of values from selected RKAnswerOptions' `value` properties. NOT SUPPORTED
+     *      Boolean: NSNumber SUPPORTED
+     *      Text: NSString SUPPORTED
+     *      Scale: NSNumber SUPPORTED
+     *      Date: RKSTDateAnswer with date components having (NSCalendarUnitEra|NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay). NOT SUPPORTED
+     *      Time: RKSTDateAnswer with date components having (NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond). NOT SUPPORTED
+     *      DateAndTime: RKSTDateAnswer with date components having (NSCalendarUnitEra|NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond). NOT SUPPORTED
+     *      Time Interval: NSNumber, containing a time span in seconds. SUPPORTED
+     */
+    __block NSString * skipToIdentifier = nil;
+    if (answer == nil || [answer isKindOfClass:[NSNumber class]] || [answer isKindOfClass:[NSString class]]) {
+        [rules enumerateObjectsUsingBlock:^(SBBSurveyRule * rule, NSUInteger idx, BOOL *stop) {
+            skipToIdentifier = [self checkRule:rule againstAnswer:answer];
+            if (skipToIdentifier) {
+                *stop = YES;
+            }
+        }];
+    }
+    if (skipToIdentifier) {
+        APCLogDebug(@"SKIPPING TO: %@", skipToIdentifier);
+    }
+    return skipToIdentifier;
+}
+
+- (NSString *) checkRule: (SBBSurveyRule*) rule againstAnswer: (id) answer
+{
+    NSString * retValue = nil;
+    NSString * operator     = [rule valueForKeyPath:kRuleOperatorKey];
+    id value                = [rule valueForKeyPath:kRuleValueKey];
+    NSString * skipToValue  = [rule valueForKeyPath:kRuleSkipToKey];
+    
+    //Skip
+    if ([operator isEqualToString:kOperatorSkip] && answer == nil) {
+        retValue = skipToValue;
+    }
+    
+    NSNumberFormatter * formatter = [NSNumberFormatter new];
+    
+    NSNumber * answerNumber;
+    NSNumber * valueNumber;
+    if ([answer isKindOfClass:[NSString class]]) {
+        answerNumber =[formatter numberFromString:answer];
+//        answerNumber = [answer isEqualToString:@"true"] ? @(YES) : answerNumber;
+//        answerNumber = [answer isEqualToString:@"false"] ? @(NO) : answerNumber;
+    }
+    else if ([answer isKindOfClass:[NSNumber class]])
+    {
+        answerNumber = answer;
+    }
+    
+    if ([value isKindOfClass:[NSString class]]) {
+        valueNumber =[formatter numberFromString:value];
+    }
+    else if ([value isKindOfClass:[NSNumber class]])
+    {
+        valueNumber = value;
+    }
+    
+    CGFloat answerDouble = answerNumber.doubleValue;
+    CGFloat valueDouble = valueNumber.doubleValue;
+    
+    //Equal
+    if ([operator isEqualToString:kOperatorEqual]) {
+        if ([answer isKindOfClass:[NSString class]] && [value isKindOfClass:[NSString class]] ) {
+            if ([answer localizedCaseInsensitiveCompare:value] == NSOrderedSame) {
+                retValue = skipToValue;
+            }
+        }
+        else
+        {
+            if (answerNumber && valueNumber) {
+                if (fabs(answerDouble - valueDouble) <= DBL_EPSILON) {
+                    retValue = skipToValue;
+                }
+            }
+        }
+    }
+    
+    //Not Equal
+    if ([operator isEqualToString:kOperatorNotEqual]) {
+        if ([answer isKindOfClass:[NSString class]] && [value isKindOfClass:[NSString class]] ) {
+            if ([answer localizedCaseInsensitiveCompare:value] != NSOrderedSame) {
+                retValue = skipToValue;
+            }
+        }
+        else
+        {
+            if (answerNumber && valueNumber) {
+                if (fabs(answerDouble - valueDouble) > DBL_EPSILON) {
+                    retValue = skipToValue;
+                }
+            }
+        }
+    }
+    
+    //Greater Than
+    if ([operator isEqualToString:kOperatorGreaterThan]) {
+        if (answerNumber && valueNumber) {
+            if ((answerDouble - valueDouble) > DBL_EPSILON) {
+                retValue = skipToValue;
+            }
+        }
+    }
+    
+    //Lesser Than
+    if ([operator isEqualToString:kOperatorLessThan]) {
+        if (answerNumber && valueNumber) {
+            if ((valueDouble - answerDouble) > DBL_EPSILON) {
+                retValue = skipToValue;
+            }
+        }
+    }
+    
+    //Greater Than or EqualTo
+    if ([operator isEqualToString:kOperatorGreaterThanEqual]) {
+        if (answerNumber && valueNumber) {
+            if ((valueDouble - answerDouble) < DBL_EPSILON) {
+                retValue = skipToValue;
+            }
+        }
+    }
+    
+    //Less Than or EqualTo
+    if ([operator isEqualToString:kOperatorGreaterThanEqual]) {
+        if (answerNumber && valueNumber) {
+            if ((answerDouble - valueDouble) < DBL_EPSILON) {
+                retValue = skipToValue;
+            }
+        }
+    }
+    
+    return retValue;
 }
 
 
