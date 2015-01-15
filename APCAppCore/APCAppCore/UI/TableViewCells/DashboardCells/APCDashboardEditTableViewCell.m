c48fe0f27a732ca622362d38ceda7a4409a16be8
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-01-15 11:52:09 -0800

    [APPLE-388] Fixing Dashboard edit mode UI issues.
    Fixing the top and bottom borders to stretch all the way across for the cells/rows.

--- APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardEditTableViewCell.m
+++ APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardEditTableViewCell.m
@@ -38,4 +38,51 @@
     self.textLabel.textColor = tintColor;
 }
 
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGFloat borderWidth = 1.0;
+    CGFloat bottomBorderWidth = 3.0;
+    
+    UIColor *borderColor = [UIColor colorWithWhite:0.973 alpha:1.000];
+    
+    // Top border
+    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, [UIColor lightGrayColor].CGColor);
+    CGContextSetLineWidth(context, borderWidth);
+    CGContextMoveToPoint(context, 0, 0);
+    CGContextAddLineToPoint(context, rect.size.width, 0);
+    CGContextStrokePath(context);
+    CGContextRestoreGState(context);
+    
+    // First Bottom Border
+    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, [UIColor lightGrayColor].CGColor);
+    CGContextSetLineWidth(context, borderWidth);
+    CGContextMoveToPoint(context, 0, rect.size.height - bottomBorderWidth);
+    CGContextAddLineToPoint(context, rect.size.width, rect.size.height - bottomBorderWidth);
+    CGContextStrokePath(context);
+    CGContextRestoreGState(context);
+    
+    // Bottom border
+    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, borderColor.CGColor);
+    CGContextSetLineWidth(context, bottomBorderWidth * 2);
+    CGContextMoveToPoint(context, 0, rect.size.height);
+    CGContextAddLineToPoint(context, rect.size.width, rect.size.height);
+    CGContextStrokePath(context);
+    CGContextRestoreGState(context);
+    
+    // Sidebar
+    CGFloat sidebarWidth = 4.0;
+    CGFloat sidbarHeight = rect.size.height - bottomBorderWidth;// - (topBorderWidth + bottomBorderWidth);
+    CGRect sidebar = CGRectMake(0, 0, sidebarWidth, sidbarHeight);
+    UIColor *sidebarColor = self.tintColor;
+    [sidebarColor setFill];
+    UIRectFill(sidebar);
+}
+
 @end
