980ecf9c342e1ec53854a5c299d9c9380d7eb273
Ron Conescu <ron.conescu@ymedialabs.com>
2015-01-22 10:27:11 -0800

    [APPLE-1243] Experimenting with Ruby and Sinatra.
    Playing with this file, trying to learn how it works.
    
    Added a new catchall URL handler, so everything
    I send to the server generates a piece of text I wrote
    (instead of text from Sinatra).
    
    Reformatted comments, changed some variable
    names, etc.
    
    Added a placeholder ReadMe file.

---
+++ LocalLoggingServerForQA/ReadMe.txt
@@ -0,0 +1,65 @@
+Hello, friendly QA people!  Ron here.
+
+Here's how to use this server.
+I'll assume you're on a Macintosh.
+
+Goal: 
+	Setup:
+		make sure "ruby" and "gem" are installed
+		install "sinatra"
+	Run:
+		run the program "localLoggingServer.rb"
+		run another program which watches for stuff to be logged
+
+------------
+Setup:
+------------
+
+1)  Open the Terminal.
+
+
+2)  Make sure "ruby" and "gem" are installed.
+
+Type these two commands:
+	ruby --version
+	gem --version
+	
+After each command, you should see a version number and other stuff, like:
+	ruby 2.0.0p481
+	gem 2.0.14
+	
+If, instead, you see messages like
+	ruby: command not found
+	gem: command not found
+	
+then let one of us programmers know, and we'll fix it.
+
+
+3)  Install "sinatra"
+
+Type this command:
+	sudo gem install sinatra
+	
+It should ask you for your password.
+
+It will then think for about 5 minutes, copying stuff from a server and printing status messages.  Wait until it says something like "Done," and seems to be waiting for more input from you.
+
+If it gives you an error, let one of us know.
+
+
+------------
+Using this
+------------
+
+1) Open two Terminal windows.
+
+2) In both window windows, change to this directory (where you're reading this file).  The easy way to do that:
+
+	type the letters "cd" (don't hit Enter)
+	hit the space bar
+	drag this folder from the Finder into the Terminal window
+	it should type the name of the folder, and all the folders above it
+	hit "Enter"
+	
+	
+(in progress.  just making a placeholder "readme" file.)
--- LocalLoggingServerForQA/localLoggingServer.rb
+++ LocalLoggingServerForQA/localLoggingServer.rb
@@ -1,75 +1,126 @@
-# Created by Dhanush Balachandran on 8/15/14.
-# Copyright (c) 2014 Y Media Labs. All rights reserved.
-
-# Sinatra Instructions
-#  In command line type
-#  $ gem install sinatra
-#  $ ruby server_mockup.rb
+# Created by Dhanush Balachandran on 2014-Aug-15.
+# Hacked up by Ron Conescu on 2015-Jan-22.
+# Copyright (c) 2015 Y Media Labs. All rights reserved.
+#
+# To install Sinatra, type this at the command line:
+# 	$ sudo gem install sinatra
+#
+# Then to run this program, simply type
+# 	$ ruby server_mockup.rb
 
 require 'sinatra'
 require 'json'
-require 'colorize'
-require 'pry'
-
 require 'fileutils'
+
 disable :logging
-
 set :bind, '0.0.0.0'
+base_url_path = '/api/v1'
+destination_directory = "~/Desktop/localLoggingServerOutput"
+output_logging_filename = "output.txt"
 
-base_path = '/api/v1'
-#Errors
-get "#{base_path}/test_fail" do
+#
+# Testing errors (?)
+#
+
+get "#{base_url_path}/test_fail" do
 	[404, {message: 'Error'}.to_json]
 end
 
-post "#{base_path}/test_fail" do
+post "#{base_url_path}/test_fail" do
 	[404, {message: 'Error'}.to_json]
 end
 
-put "#{base_path}/test_fail" do
+put "#{base_url_path}/test_fail" do
 	[404, {message: 'Error'}.to_json]
 end
 
-#Maintenance
-get "#{base_path}/server_maintenance" do
+
+#
+# Maintenance
+# 
+
+get "#{base_url_path}/server_maintenance" do
 	[503, {message: 'Server Under Maintenance'}.to_json]
 end
 
-#File Upload
-post "#{base_path}/upload/:filename" do
-    # userdir = "/tmp/upload_files"
-    userdir = "./"
-	  FileUtils.mkdir_p(userdir)
-	  puts "#{params}"
-	  filename = File.join(userdir, params[:filename])
-	  datafile = params[:filedata]
-	  FileUtils.copy(datafile[:tempfile], filename)
-	  "wrote to #{filename}\n"
-	  200
-end
 
-#Authentication
-post "#{base_path}/auth/signUp" do
+#
+# File Upload
+#
+
+post "#{base_url_path}/upload/:filename" do
+	FileUtils.mkdir_p(destination_directory)
+	puts "#{params}"
+	filename = File.join(destination_directory, params[:filename])
+	datafile = params[:filedata]
+	FileUtils.copy(datafile[:tempfile], filename)
+	"wrote to #{filename}\n"
 	200
 end
 
-post "#{base_path}/auth/signIn" do
-	json = JSON.parse request.body.read
-	[200, {username: json[:username], sessionToken: 'sessionToken', consented: true, authenticated: true}.to_json]
+
+#
+# Authentication
+#
+
+post "#{base_url_path}/auth/signUp" do
+	200
 end
 
-#Catch All
-get "#{base_path}/*" do
+post "#{base_url_path}/auth/signIn" do
+	json = JSON.parse request.body.read
+	[200, { username: 		json[:username],	\
+			sessionToken: 	'sessionToken',		\
+			consented: 		true,				\
+			authenticated: 	true				\
+		  }.to_json]
+end
+
+
+#
+# Catch All
+#
+
+get "#{base_url_path}/*" do
 	[200, {endpoint: {hello: 'world'}}.to_json] 
 end
 
-post "#{base_path}/*" do 
+post "#{base_url_path}/*" do 
 	json = JSON.parse request.body.read
 	puts json["log"].to_s.colorize(:cyan)
 	[200, {endpoint: params, body: json }.to_json] 
 end
 
-put "#{base_path}/*" do 
+put "#{base_url_path}/*" do 
 	json = JSON.parse request.body.read
 	[200, {endpoint: params, body: json }.to_json] 
 end
+
+# This works:  catches all requests that didn't
+# meet one of the above criteria.  ORDER MATTERS:
+# if this rule goes first, the other rules don't
+# run.
+get "*" do
+	[200, {error: "I didn't understand that command."}.to_json]
+end
+
+
+#
+# Experimenting
+#
+
+# Doesn't work yet.
+get "#{base_url_path}/testWrite/:content" do
+	FileUtils.mkdir_p(destination_directory)
+	puts "#{params}"
+	filename = File.join(destination_directory, output_logging_filename)
+	datafile = params[:content]
+	FileUtils.append(datafile[:tempfile], filename)
+	"wrote to #{filename}\n"
+	200
+end
+
+
+
+
+
