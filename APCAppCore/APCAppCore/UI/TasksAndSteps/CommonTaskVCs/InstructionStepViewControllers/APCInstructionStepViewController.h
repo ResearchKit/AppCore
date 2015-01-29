bc8f271b171e6512a01a7b651c9ad6cf333c5c5d
Justin Warmkessel <justin.w@ymedialabs.com>
2015-01-29 14:56:33 -0800

    Revert "Updating instructions controller get started button."
    This reverts commit fb4569a633339ed9d1cf18c4341095e3bb871060.

--- APCAppCore/APCAppCore/UI/TasksAndSteps/CommonTaskVCs/InstructionStepViewControllers/APCInstructionStepViewController.h
+++ APCAppCore/APCAppCore/UI/TasksAndSteps/CommonTaskVCs/InstructionStepViewControllers/APCInstructionStepViewController.h
@@ -6,14 +6,14 @@
 // 
  
 #import "APCStepViewController.h"
-@class APCButton;
+
 @interface APCInstructionStepViewController : APCStepViewController
 
 @property (nonatomic, strong) NSArray * imagesArray;
 @property (nonatomic, strong) NSArray * headingsArray;
 @property (nonatomic, strong) NSArray * messagesArray;
 
-@property (weak, nonatomic) IBOutlet APCButton *gettingStartedButton;
+@property (weak, nonatomic) IBOutlet UIButton *gettingStartedButton;
 @property (strong, nonatomic)        UIView   *accessoryContent;
 
 @end
