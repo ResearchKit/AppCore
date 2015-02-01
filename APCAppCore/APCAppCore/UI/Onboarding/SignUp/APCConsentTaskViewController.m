d34da1c6e3fbc652601bf3fa8d0a2564e11eb4d4
Chris Wardell <chris.wardell@Chris-Wardells-MacBook-Pro.local>
2015-02-01 12:38:37 -0800

    [AsthmaRehosting] Add required protocol methods

--- APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCConsentTaskViewController.m
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCConsentTaskViewController.m
@@ -23,14 +23,17 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark Required Protocol Methods for Abstract Class
+-(void)taskViewControllerDidComplete:(RKSTTaskViewController *)taskViewController{
+    
 }
-*/
+
+-(void)taskViewController:(RKSTTaskViewController *)taskViewController didFailOnStep:(RKSTStep *)step withError:(NSError *)error{
+    
+}
+
+-(void)taskViewControllerDidCancel:(RKSTTaskViewController *)taskViewController{
+    
+}
 
 @end
