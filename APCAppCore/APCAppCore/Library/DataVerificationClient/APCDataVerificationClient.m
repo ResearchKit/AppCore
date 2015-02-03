492e6af081e8f8cb3f5754b3cb55178e3c8d1014
Ron Conescu <ron.conescu@ymedialabs.com>
2015-02-03 08:52:43 -0800

    [APPLE-1243] Works with untethered phones.
    Added a little convention for adding IP
    addresses where the server is running
    (our office laptop computers).  Made sure
    it works on my personal phone.

--- APCAppCore/APCAppCore/Library/DataVerificationClient/APCDataVerificationClient.m
+++ APCAppCore/APCAppCore/Library/DataVerificationClient/APCDataVerificationClient.m
@@ -24,11 +24,30 @@
 
 
 /**
- Change this to the computer where you're running the server.
- */
-//static NSString * const DATA_VERIFICATION_SERVER_IP_ADDRESS = @"10.5.28.84";		// Ron's Mac
-static NSString * const DATA_VERIFICATION_SERVER_IP_ADDRESS = @"127.0.0.1";		// If you're using the Simulator
+ To make this app work with the DataVerificationServer,
+ uncomment the entry for the computer where the server
+ is running.  If you're using the Simulator, uncomment
+ the "localhost" line (127.0.0.1).  Please set this back
+ to "localhost" before committing to Git.
 
+ This is safe becase the surrounding #define means
+ this code will never ship in production.  The #defined
+ item is triggered by a special Xcode Scheme.
+ 
+ This is set up so you can easily type command-"/" to
+ comment or uncomment certain lines.
+ */
+static NSString * const DATA_VERIFICATION_SERVER_IP_ADDRESS = (
+//															   @"10.5.28.84"	// Ron's Mac
+															   @"127.0.0.1"		// "localhost" - if you're using the Simulator
+															   );
+
+
+/**
+ This will probably never change, but, still.
+ Don't modify this unless you're also changing
+ the matching Ruby code, please.
+ */
 static NSInteger const DATA_VERIFICATION_SERVER_PORT_NUMBER = 4567;
 
 
