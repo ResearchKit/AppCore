4ec532fb1a1befbf2e51df2420d0942888a1eb36
Ron Conescu <ron.conescu@ymedialabs.com>
2015-02-06 11:36:05 -0800

    [APPLE-1587] Trying 9 ways to get app name.
    The crash was:  I was inserting a nil into an
    NSDictionary.
    
    The "nil" was from an attempt to get the app's
    human-readable name (the name of the app
    as visible on the phone's home screen). For
    some reason, the version I had worked in
    Parkinson's, but not Asthma.
    
    This commit:
    
    -  Created a utility class which tries 9 different ways
       to retrieve the app name, stopping as soon as it
       finds one.  It always returns a non-nil, non-empty
       string, even if it has to say, "I couldn't find that
       item."
    
    -  Does the same safety checks for appVersion and
       phoneInfo.
    
    -  Reorganized the methods in APCDataArchiver to
       put the -init methods next to each other.  At first,
       I found their previous organization a little confusing:
       I didn't realize there was a large, complex -init
       method in a different visual section of the file,
       and became concerned that the crash was because
       I wasn't initializing things properly.  If this was a bad
       choice of organizational schemes, I apologize, and
       I'll be glad to change it back.

---
+++ APCAppCore/APCAppCore/Library/Objects/APCUtilities.m
@@ -0,0 +1,127 @@
+//
+//  APCUtilities.m
+//  AppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCUtilities.h"
+#import "APCDeviceHardware.h"
+
+
+/**
+ These are fixed per launch of the app.  They're also
+ small.  So cache them -- partly because we're still figuring
+ out how to find them (and having trouble in some apps).
+ */
+static NSString *_appName = nil;
+static NSString *_appVersion = nil;
+static NSString *_deviceDescription = nil;
+
+static NSString * const APP_NAME_IF_CANT_DETECT		= @"CouldNotRetrieveAppName";
+static NSString * const VERSION_IF_CANT_DETECT		= @"???";
+static NSString * const BUILD_IF_CANT_DETECT		= @"???";
+static NSString * const DEVICE_INFO_IF_CANT_DETECT	= @"CouldNotRetrieveDeviceInfo";
+
+
+@implementation APCUtilities
+
++ (NSString *) deviceDescription
+{
+	if (_deviceDescription == nil)
+	{
+		_deviceDescription = [self cleanString: APCDeviceHardware.platformString] ?: DEVICE_INFO_IF_CANT_DETECT;
+	}
+
+	return _deviceDescription;
+}
+
++ (NSString *) phoneInfo
+{
+	return self.deviceDescription;
+}
+
++ (NSString *) appVersion
+{
+	if (_appVersion == nil)
+	{
+		NSString *version = [self cleanString: NSBundle.mainBundle.infoDictionary [@"CFBundleShortVersionString"]] ?: VERSION_IF_CANT_DETECT;
+		NSString *build   = [self cleanString: NSBundle.mainBundle.infoDictionary [@"CFBundleVersion"]] ?: BUILD_IF_CANT_DETECT;
+		
+		_appVersion	= [NSString stringWithFormat: @"version %@, build %@", version, build];
+	}
+
+	return _appVersion;
+}
+
++ (NSString *) appName
+{
+	if (_appName == nil)
+	{
+		NSBundle *mainBundle = [NSBundle mainBundle];
+		NSDictionary *bundleInfo = mainBundle.infoDictionary;
+		NSDictionary *localizedBundleInfo = mainBundle.localizedInfoDictionary;
+
+		/*
+		 How do we get the app name, officially?
+
+		 Here are a bunch of attempts.  Some work in some of
+		 our apps, and others don't.  I don't yet know why
+		 the ones that work actually work.
+		 
+		 -----------------
+		 ABOUT THIS SYNTAX
+		 -----------------
+		 The syntax below is a short way of writing 10 "if"
+		 statements to evaluate a bunch of strings.  Each line
+		 works like this (here's line 2 as an example):
+		 -
+		 -			[self cleanString: bundleInfo [@"CFBundleDisplayName"]] ?:
+		 -			                   11111111111111111111111111111111111
+		 -			 22222222222222222
+		 -			                                                        3    <--- look
+		 -			                                                         4   <---   here
+		 -	1.	try to extract the string from somewhere
+		 -	2.	clean it (trim, set to nil if empty)
+		 -	3.	if it's non-nil and non-empty, use it
+		 -	4.	otherwise, try the next line
+		 */
+
+		_appName = ([self cleanString: bundleInfo [@"CFBundleName"]] ?:				// yes in Asthma
+					[self cleanString: bundleInfo [@"CFBundleDisplayName"]] ?:		// yes in Parkinson's, no in Asthma
+					[self cleanString: bundleInfo [@"CFBundleExecutable"]] ?:		// yes in Asthma
+
+					// Apple:  "please use this method, 'cuz it gets the localized version if one is available"
+					[self cleanString: [mainBundle objectForInfoDictionaryKey: @"CFBundleName"]] ?:				// yes in Asthma
+					[self cleanString: [mainBundle objectForInfoDictionaryKey: @"CFBundleDisplayName"]] ?:		// no in Asthma
+					[self cleanString: [mainBundle objectForInfoDictionaryKey: @"CFBundleExecutable"]] ?:		// yes in Asthma
+
+					[self cleanString: localizedBundleInfo [@"CFBundleName"]] ?:			// no in Asthma
+					[self cleanString: localizedBundleInfo [@"CFBundleDisplayName"]] ?:		// no in Asthma
+					[self cleanString: localizedBundleInfo [@"CFBundleExecutable"]] ?:		// no in Asthma
+
+					APP_NAME_IF_CANT_DETECT);
+	}
+
+	return _appName;
+}
+
+/**
+ Trims whitespace from someString and returns it.
+ If the trimmed string has length 0, returns nil.
+ This lets us do simplified "if" statements, evaluating
+ the string for its "truth" value.
+ */
++ (NSString *) cleanString: (NSString *) someString
+{
+	NSString *result = [someString stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+	if (result.length == 0)
+	{
+		result = nil;
+	}
+
+	return result;
+}
+
+@end
