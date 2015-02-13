32915c2f08b7121e5303fd30c1419204269f0854
Ron Conescu <ron.conescu@ymedialabs.com>
2015-02-13 15:09:40 -0800

    [APPLE-1780] Preventing crash when receive "unreasonable" height.
    Before this fix:  if the user supplied an "unreasonable" height in the Health system on the phone, the app would crash.
    
    This fix prevents the crash:  if the height is outside the known list of heights, we'll choose a "default" height (5 feet tall).
    
    Please note:  this fix is only part of the fix to a bigger bug.  The bigger bug is that our apps will *increase* the user's height each time we reinstall the app.  After enough reinstalls, the height becomes "unreasonable," and this secondary bug kicks in, causing the app to crash.  This commit fixes the bug which causes the crash, but not the bigger bug which increases the user's height.

--- APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpMedicalInfoViewController.m
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpMedicalInfoViewController.m
@@ -155,24 +155,37 @@
                     field.detailDiscloserStyle = YES;
                     field.textAlignnment = NSTextAlignmentRight;
                     field.pickerData = [APCUser heights];
+
+					NSInteger defaultIndexOfMyHeightInFeet = 5;
+					NSInteger defaultIndexOfMyHeightInInches = 0;
+					NSInteger indexOfMyHeightInFeet = defaultIndexOfMyHeightInFeet;
+					NSInteger indexOfMyHeightInInches = defaultIndexOfMyHeightInInches;
+
                     if (self.user.height) {
                         double heightInInches = round([APCUser heightInInches:self.user.height]);
                         
-                        // When a height is returned from HealthKit as anything 12 inches or less,
-                        // we will default it to 12 inches.
-                        if (heightInInches < 12) {
-                            heightInInches = 12;
-                        }
-                        
                         NSString *feet = [NSString stringWithFormat:@"%d'", (int)heightInInches/12];
                         NSString *inches = [NSString stringWithFormat:@"%d''", (int)heightInInches%12];
-                        
-                        field.selectedRowIndices = @[ @([field.pickerData[0] indexOfObject:feet]), @([field.pickerData[1] indexOfObject:inches]) ];
+
+						NSArray *allPossibleHeightsInFeet = field.pickerData [0];
+						NSArray *allPossibleHeightsInInches = field.pickerData [1];
+
+						indexOfMyHeightInFeet = [allPossibleHeightsInFeet indexOfObject: feet];
+						indexOfMyHeightInInches = [allPossibleHeightsInInches indexOfObject: inches];
+
+						if (indexOfMyHeightInFeet == NSNotFound)
+						{
+							indexOfMyHeightInFeet = defaultIndexOfMyHeightInFeet;
+						}
+
+						if (indexOfMyHeightInInches == NSNotFound)
+						{
+							indexOfMyHeightInInches = defaultIndexOfMyHeightInInches;
+						}
                     }
-                    else {
-                        field.selectedRowIndices = @[ @(5), @(0) ];
-                    }
-                    
+
+					field.selectedRowIndices = @[ @(indexOfMyHeightInFeet), @(indexOfMyHeightInInches) ];
+
                     APCTableViewRow *row = [APCTableViewRow new];
                     row.item = field;
                     row.itemType = kAPCUserInfoItemTypeHeight;
