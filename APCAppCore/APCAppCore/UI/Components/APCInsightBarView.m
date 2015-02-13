06ef8ee7dcf841d37c9b6a4b1150737cef85126a
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-02-12 17:32:24 -0800

    [APPLE-1733] Properly drawing the bar for insight.

--- APCAppCore/APCAppCore/UI/Components/APCInsightBarView.m
+++ APCAppCore/APCAppCore/UI/Components/APCInsightBarView.m
@@ -19,23 +19,25 @@
     CGFloat goodWidth = 0;
     CGFloat badWidth  = 0;
     
-    if ([self.goodDayValue doubleValue] > [self.badDayValue doubleValue]) {
-        goodWidth = rect.size.width;
-        badWidth = rect.size.width * ([self.badDayValue doubleValue] / [self.goodDayValue doubleValue]);
-    } else {
-        goodWidth = rect.size.width * ([self.goodDayValue doubleValue] / [self.badDayValue doubleValue]);
-        badWidth = rect.size.width;
+    if (([self.goodDayValue doubleValue] != 0) || ([self.badDayValue doubleValue] != 0)) {
+        if ([self.goodDayValue doubleValue] > [self.badDayValue doubleValue]) {
+            goodWidth = rect.size.width;
+            badWidth = rect.size.width * ([self.badDayValue doubleValue] / [self.goodDayValue doubleValue]);
+        } else {
+            goodWidth = rect.size.width * ([self.goodDayValue doubleValue] / [self.badDayValue doubleValue]);
+            badWidth = rect.size.width;
+        }
+        
+        // Good Bar
+        CGRect barGood = CGRectMake(0, 0, goodWidth, barHeight);
+        [goodBarColor setFill];
+        UIRectFill(barGood);
+        
+        // Bad Bar
+        CGRect barBad = CGRectMake(0, rect.size.height - barHeight, badWidth, barHeight);
+        [badBarColor setFill];
+        UIRectFill(barBad);
     }
-    
-    // Good Bar
-    CGRect barGood = CGRectMake(0, 0, goodWidth, barHeight);
-    [goodBarColor setFill];
-    UIRectFill(barGood);
-    
-    // Bad Bar
-    CGRect barBad = CGRectMake(0, rect.size.height - barHeight, badWidth, barHeight);
-    [badBarColor setFill];
-    UIRectFill(barBad);
 }
 
 - (void)setGoodDayValue:(NSNumber *)goodDayValue
