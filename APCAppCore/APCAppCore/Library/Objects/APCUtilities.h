eaffd2051aad2228a2c43c2a10e1ed931e13b8a6
Ron Conescu <ron.conescu@ymedialabs.com>
2015-02-13 12:19:57 -0800

    [APPLE-1614] Cleaning up APCLog.h and .m.
    - Deleted old experiments (my original thoughts about this file).  We're now only using the version Dhanush requested, which (often) wraps Flurry calls.
    - Moved the main macros to the top of the .h file, for readability.
    - Added some documentation.
    - Declared some constant strings as actual C constants.  Didn't make them public 'cuz there's no need, yet, and when I tried to use the real logging statements, it was confusing to have those (useless) constants appear in the text-completion menu.
    - Moved a general-purpose macro (for converting variadic arguments into a string) into APCUtilities.h.
    - Removed my and Dhanush's names.

--- APCAppCore/APCAppCore/Library/Objects/APCUtilities.h
+++ APCAppCore/APCAppCore/Library/Objects/APCUtilities.h
@@ -53,3 +53,96 @@
 
 
 @end
+
+
+
+// ---------------------------------------------------------
+#pragma mark - Macro:  converting varArgs into a string
+// ---------------------------------------------------------
+
+/**
+ This macro converts a bunch of "..." arguments into an NSString.
+ 
+ (Other keywords to find this chunk of code:  va_args,
+ varargs, vaargs, variadic arguments, variadic macro,
+ dotdotdot, ellipsis, three dots)
+
+ Note that this macro requires ARC.  (To use it without ARC,
+ edit the macro to call "autorelease" on formattedMessage before
+ returning it.)
+
+
+ To use it: 
+ 
+ First, create a method that ENDS with a "...", like this:
+ 
+		- (void) printMyStuff: (NSString *) messageFormat, ...
+		{
+		}
+ 
+ Inside that method, call this macro, passing it the string
+ you want to use as a formatting string.  Using the above
+ example, it might be:
+ 
+		- (void) printMyStuff: (NSString *) messageFormat, ...
+		{
+			NSString extractedString = stringFromVariadicArgumentsAndFormat( messageFormat );
+ 
+			//
+			// now use the extractedString.  For example:
+			//
+			NSLog (@"That string was: %@", extractedString);
+		}
+
+ Behind the scenes, this macro extracts the parameters from
+ that "...", takes your formatting string, and passes them
+ all to +[NSString stringWithFormat], giving you a normally-
+ formatted string as a result.
+ 
+ This macro is identical to typing the following mess into
+ the same method:
+ 
+	va_list arguments;
+	va_start (arguments, format);
+	NSString *formattedMessage = [[NSString alloc] initWithFormat: format
+													    arguments: arguments];
+	va_end (arguments);
+ 
+ ...and then using the string "formattedMessage" somewhere.
+ 
+ If you're interested:  this macro "returns" a value by wrapping
+ the whole thing in a ({ ... }) and them simply putting the value
+ on a line by itself at the end.
+ 
+ References:
+
+ -	Extracting the variadic arguments (the "..." parameter) into an array we pass to NSString:
+	http://stackoverflow.com/questions/1420421/how-to-pass-on-a-variable-number-of-arguments-to-nsstrings-stringwithformat
+
+ -	"Returning" a value from a macro:
+	http://stackoverflow.com/questions/2679182/have-macro-return-a-value
+
+ -	More ways to get to the variadic arguments:
+	https://developer.apple.com/library/mac/qa/qa1405/_index.html
+ 
+ -	Well-written, general-purpose documentation about writing macros,
+	which talks about the rules for defining macro "functions," using
+	the trailing "\", and many cool tricks and rules:
+	https://gcc.gnu.org/onlinedocs/cpp/Macros.html
+ */
+#define NSStringFromVariadicArgumentsAndFormat( formatString )				\
+	({																		\
+		NSString *formattedMessage = nil;									\
+		va_list arguments;													\
+		va_start (arguments, formatString);									\
+		formattedMessage = [[NSString alloc] initWithFormat: formatString	\
+												  arguments: arguments];	\
+		va_end (arguments);													\
+		formattedMessage;													\
+	})
+
+
+
+
+
+
