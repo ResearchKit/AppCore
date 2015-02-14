d01451baa0ecb2bdc65ae334960689519d96d919
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-02-14 11:58:25 -0800

    Adding Food Insight custom cell.

---
+++ APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardFoodInsightTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  APCDashboardFoodInsightTableViewCell.h
+//  APCAppCore
+//
+//  Created by Farhan Ahmed on 2/14/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APCDashboardFoodInsightTableViewCell : UITableViewCell
+
+@end
---
+++ APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardFoodInsightTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  APCDashboardFoodInsightTableViewCell.m
+//  APCAppCore
+//
+//  Created by Farhan Ahmed on 2/14/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCDashboardFoodInsightTableViewCell.h"
+
+@implementation APCDashboardFoodInsightTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
