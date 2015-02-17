cf7201d0851cac162c22abf2afe6edada4a51df6
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2015-02-17 07:14:07 -0800

    [APPLE-1889] Fixed for iPhone 5 simulator. Added "i386" case check in the framework_switcher code.

--- APCAppCore/APCAppCore/Resources/framework_switcher.sh
+++ APCAppCore/APCAppCore/Resources/framework_switcher.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 rm -f "${SRCROOT}/ResearchKit/ResearchKit.framework"
-if [ "$ARCHS" == "x86_64" ]
+if [["$ARCHS" == "x86_64"]]
 then
+	echo "------Linking x86 Framework-------"
+	ln -s "x86/ResearchKit.framework"  "ResearchKit/ResearchKit.framework"
+elif [[ "$ARCHS" == "i386" ]]; then
 	echo "------Linking x86 Framework-------"
 	ln -s "x86/ResearchKit.framework"  "ResearchKit/ResearchKit.framework"
 else
