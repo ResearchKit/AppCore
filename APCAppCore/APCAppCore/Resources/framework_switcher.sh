06ddb13f423bf1cff07aa2add54191a9f88e0130
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2015-02-17 07:20:50 -0800

    [APPLE-1889] More cleanup

--- APCAppCore/APCAppCore/Resources/framework_switcher.sh
+++ APCAppCore/APCAppCore/Resources/framework_switcher.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 rm -f "${SRCROOT}/ResearchKit/ResearchKit.framework"
-if [["$ARCHS" == "x86_64"]]
-then
+if [[ "$ARCHS" == "x86_64" ]];then
 	echo "------Linking x86 Framework-------"
 	ln -s "x86/ResearchKit.framework"  "ResearchKit/ResearchKit.framework"
 elif [[ "$ARCHS" == "i386" ]]; then
