e43dcaed1fbf0fb23f54ce908e5038c60086c302
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2015-02-20 10:38:50 +0530

    [APPLE-1929] Add styling to PiegraphCell

--- APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardPieGraphTableViewCell.m
+++ APCAppCore/APCAppCore/UI/TableViewCells/DashboardCells/APCDashboardPieGraphTableViewCell.m
@@ -7,11 +7,24 @@
  
 #import "APCDashboardPieGraphTableViewCell.h"
 #import "UIFont+APCAppearance.h"
+#import "UIColor+APCAppearance.h"
 
 @implementation APCDashboardPieGraphTableViewCell
 
 - (void)awakeFromNib {
+    
     [super awakeFromNib];
+    
+    [self setupAppearance];
+}
+
+- (void)setupAppearance
+{
+    self.subTitleLabel.font = [UIFont appRegularFontWithSize:16.0f];
+    self.subTitleLabel.textColor = [UIColor appSecondaryColor3];
+    
+    self.subTitleLabel2.font = [UIFont appRegularFontWithSize:16.0f];
+    self.subTitleLabel2.textColor = [UIColor appSecondaryColor3];
 }
 
 
