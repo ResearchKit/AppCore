df9bd0ddb35ebb89ebc768538f698e5f9d7ce80c
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-02-21 09:49:15 -0800

    [APPLE-1967] Moved Setup of ScrollView Pages into the ScrollView Pages Themselves

--- APCAppCore/APCAppCore/Library/MedicationTrackingAppComponent/APCMedicationTrackerMedicationsDisplayView.h
+++ APCAppCore/APCAppCore/Library/MedicationTrackingAppComponent/APCMedicationTrackerMedicationsDisplayView.h
@@ -7,6 +7,19 @@
 
 #import <UIKit/UIKit.h>
 
+@class  APCMedicationTrackerMedicationsDisplayView;
+@class  APCLozengeButton;
+
+@protocol  APCMedicationTrackerMedicationsDisplayViewDelegate  <NSObject>
+
+- (void)displayView:(APCMedicationTrackerMedicationsDisplayView *)displayView lozengeButtonWasTapped:(APCLozengeButton *)lozenge;
+
+@end
+
 @interface APCMedicationTrackerMedicationsDisplayView : UIView
+
+@property  (nonatomic, strong)  NSArray  *medicationModels;
+
+@property  (nonatomic, weak)  id <APCMedicationTrackerMedicationsDisplayViewDelegate>  delegate;
 
 @end
