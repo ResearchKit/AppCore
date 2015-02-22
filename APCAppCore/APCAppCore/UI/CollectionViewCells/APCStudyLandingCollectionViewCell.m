9347eba218d44f3ecbbf60661023c63f20c57433
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2015-02-23 00:29:53 +0530

    [APPLE-2075] Add new landing Page

---
+++ APCAppCore/APCAppCore/UI/CollectionViewCells/APCStudyLandingCollectionViewCell.m
@@ -0,0 +1,61 @@
+//
+//  APCStudyLandingCollectionViewCell.m
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCStudyLandingCollectionViewCell.h"
+#import "UIColor+APCAppearance.h"
+#import "UIFont+APCAppearance.h"
+
+NSString *const kAPCStudyLandingCollectionViewCellIdentifier = @"APCStudyLandingCollectionViewCell";
+
+@implementation APCStudyLandingCollectionViewCell
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    [self setupAppearance];
+}
+
+- (void)setupAppearance
+{
+    self.logoImageView.image = [UIImage imageNamed:@"logo_disease_large"];
+    
+    self.titleLabel.textColor = [UIColor appSecondaryColor1];
+    self.titleLabel.font = [UIFont appLightFontWithSize:36.f];
+    [self.titleLabel setAdjustsFontSizeToFitWidth:YES];
+    self.titleLabel.minimumScaleFactor = 0.8;
+    
+    self.subTitleLabel.textColor = [UIColor appSecondaryColor1];
+    self.subTitleLabel.font = [UIFont appMediumFontWithSize:17.f];
+    
+    self.swipeLabel.textColor = [UIColor appSecondaryColor3];
+    self.swipeLabel.font = [UIFont appMediumFontWithSize:15.f];
+    
+    [self.emailConsentButton setTitle:NSLocalizedString(@"Email Consent Document", @"Email Consent Document") forState:UIControlStateNormal];
+    self.emailConsentButton.titleLabel.textColor = [UIColor appPrimaryColor];
+    
+    [self.readConsentButton setTitle:NSLocalizedString(@"Read Consent Document", @"Read Consent Document") forState:UIControlStateNormal];
+    self.readConsentButton.titleLabel.textColor = [UIColor appPrimaryColor];
+
+}
+
+
+- (IBAction)readConsent:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(studyLandingCollectionViewCellReadConsent:)]) {
+        [self.delegate studyLandingCollectionViewCellReadConsent:self];
+    }
+}
+
+- (IBAction)emailConsent:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(studyLandingCollectionViewCellEmailConsent:)]) {
+        [self.delegate studyLandingCollectionViewCellEmailConsent:self];
+    }
+}
+
+@end
