1dea7805692a981d17c6cc3a102186528a763510
Ramsundar Shandilya <ram.shandilya@ymedialabs.com>
2015-02-23 16:05:46 +0530

    [APPLE-2053] Forgot Password screen is now presented modally.

--- APCAppCore/APCAppCore/UI/Onboarding/SignIn/APCForgotPasswordViewController.h
+++ APCAppCore/APCAppCore/UI/Onboarding/SignIn/APCForgotPasswordViewController.h
@@ -6,12 +6,18 @@
 // 
  
 #import <UIKit/UIKit.h>
+#import "APCButton.h"
 
 @interface APCForgotPasswordViewController : UITableViewController <UITextFieldDelegate>
 
 @property (nonatomic, weak) IBOutlet UITextField *emailTextField;
 
 @property (weak, nonatomic) IBOutlet UILabel *emailMessageLabel;
+@property (weak, nonatomic) IBOutlet APCButton *resetButton;
+
 - (void) sendPassword;
+
+- (IBAction)resetPassword:(id)sender;
+- (IBAction)cancel:(id)sender;
 
 @end
