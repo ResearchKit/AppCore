f705582a937e1350ca9f89a22e0142b2b4655837
Justin Warmkessel <justin.w@ymedialabs.com>
2015-02-24 14:43:43 -0800

    [APPLE-1153] Overriding methods for core motion tracker.

--- APCAppCore/APCAppCore/Library/DataArchiverAndCollector/Trackers/APCCoreMotionTracker.m
+++ APCAppCore/APCAppCore/Library/DataArchiverAndCollector/Trackers/APCCoreMotionTracker.m
@@ -7,6 +7,29 @@
 
 #import "APCCoreMotionTracker.h"
 
+static NSString *const kLastUsedTimeKey = @"APHLastUsedTime";
+
 @implementation APCCoreMotionTracker
 
+- (void) startTracking
+{
+    NSDate *lastUsedTime = [[NSUserDefaults standardUserDefaults] objectForKey:kLastUsedTimeKey];
+    if (!lastUsedTime) {
+        
+    }
+}
+
+- (void) stopTracking
+{
+    //Abstract implementation
+}
+
+- (void) updateTracking
+{
+    //Abstract implementation
+}
+
+//NSDate *currentTime = [NSDate date];
+//[[NSUserDefaults standardUserDefaults] setObject:currentTime forKey:kLastUsedTimeKey];
+//NSDate *lastUsedTime = [[NSUserDefaults standardUserDefaults] objectForKey:kLastUsedTimeKey];
 @end
