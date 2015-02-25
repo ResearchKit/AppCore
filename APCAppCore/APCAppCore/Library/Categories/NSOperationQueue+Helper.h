c0e37d2c770434413d177f1aa0155590f4d97a88
Ron Conescu <ron.conescu@ymedialabs.com>
2015-02-25 11:49:40 -0800

    [APPLE-1697] Making my operation-queue priority explicit.
    I wrote a category on NSOperationQueue which blindly accepted the system's default priority.  Now I'm setting it explicitly — to the same thing, for now, until we get a chance to play with it.  Wrote a generalized method to set the priority however we want, but I'm not exposing it yet, because I don't want people accidentally setting the priority to "high" and competing with the main thread.

--- APCAppCore/APCAppCore/Library/Categories/NSOperationQueue+Helper.h
+++ APCAppCore/APCAppCore/Library/Categories/NSOperationQueue+Helper.h
@@ -7,8 +7,21 @@
 
 #import <Foundation/Foundation.h>
 
+
 @interface NSOperationQueue (Helper)
 
+/**
+ Creates an operation queue with medium priority.
+ Designed (by Apple) for doing user-requested things
+ in the background.
+ 
+ Note:  internally, inside this file, we actually specify
+ the priority we want to use.  There's also a method
+ which lets us specify the priority explicitly.  I'm not
+ exposing that, yet, until we're sure we need and want
+ it:  allowing ourselves to set priorities to random things
+ might cause more trouble than we want to have to debug.
+ */
 + (instancetype) sequentialOperationQueueWithName: (NSString *) name;
 
 @end
--- APCAppCore/APCAppCore/Library/Categories/NSOperationQueue+Helper.m
+++ APCAppCore/APCAppCore/Library/Categories/NSOperationQueue+Helper.m
@@ -7,13 +7,79 @@
 
 #import "NSOperationQueue+Helper.h"
 
+
+
+
+/**
+ Priorities.  These correspond directly to Apple's queue
+ priorities; I'm just making them more readable, in terminology
+ we're used to.
+ */
+typedef enum : NSUInteger {
+
+    /** Same priority as stuff on the main queue.  Be verrrrrrry
+     wary of using this.  You'll compete with the main queue --
+     user animations and whatnot. */
+    APCOperationQueuePriorityHighest,
+
+    /**
+     Pretty important stuff.  More important than average,
+     not as important as user animations.
+     */
+    APCOperationQueuePriorityHigh,
+
+    /** Designed by Apple for doing user stuff in the background. */
+    APCOperationQueuePriorityMedium,
+
+    /** Designed for doing truly background tasks, like
+     downloading a large file:  it could take "forever"
+     anyway, so a few extra milliseconds (or maybe even
+     seconds) won't hurt. */
+    APCOperationQueuePriorityLow
+
+} APCOperationQueuePriority;
+
+
+
+
 @implementation NSOperationQueue (Helper)
 
 + (instancetype) sequentialOperationQueueWithName: (NSString *) name
 {
+    return [self sequentialOperationQueueWithName: name
+                                         priority: APCOperationQueuePriorityMedium];
+}
+
++ (instancetype) sequentialOperationQueueWithName: (NSString *) name
+                                         priority: (APCOperationQueuePriority) priority
+{
     NSOperationQueue *queue = [NSOperationQueue new];
     queue.name = name;
     queue.maxConcurrentOperationCount = 1;
+
+    NSOperationQualityOfService underlyingPriority = NSOperationQualityOfServiceBackground;
+
+    switch (priority)
+    {
+        case APCOperationQueuePriorityHighest:
+            underlyingPriority = NSOperationQualityOfServiceUserInteractive;
+            break;
+
+        case APCOperationQueuePriorityHigh:
+            underlyingPriority = NSOperationQualityOfServiceUserInitiated;
+            break;
+
+        default:
+        case APCOperationQueuePriorityMedium:
+            underlyingPriority = NSOperationQualityOfServiceUtility;
+            break;
+
+        case APCOperationQueuePriorityLow:
+            underlyingPriority = NSOperationQualityOfServiceBackground;
+            break;
+    }
+
+    queue.qualityOfService = underlyingPriority;
     return queue;
 }
 
