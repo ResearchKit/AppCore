030d3f3c73003b7420e404d2424e43680d7298ee
Chris Wardell <chris.wardell@Chris-Wardells-MacBook-Pro.local>
2015-02-25 09:03:02 -0800

    [APPLE-2157] Add InhalerUsageTracker to project

---
+++ APCAppCore/APCAppCore/Library/DataArchiverAndCollector/Trackers/APCInhalerUsageTracker.h
@@ -0,0 +1,12 @@
+//
+//  APCInhalerUsageTracker.h
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <APCAppCore/APCAppCore.h>
+
+@interface APCInhalerUsageTracker : APCDataTracker
+
+@end
---
+++ APCAppCore/APCAppCore/Library/DataArchiverAndCollector/Trackers/APCInhalerUsageTracker.m
@@ -0,0 +1,18 @@
+//
+//  APCInhalerUsageTracker.m
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCInhalerUsageTracker.h"
+#import <HKHealthStore+APCExtensions.h>
+
+@implementation APCInhalerUsageTracker
+
+-(instancetype)initWithIdentifier:(NSString *)identifier{
+    self = [super initWithIdentifier:identifier];
+    return self;
+}
+
+@end
