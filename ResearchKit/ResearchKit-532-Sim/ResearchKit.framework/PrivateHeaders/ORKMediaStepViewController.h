ff1a7c44e5d78cbf847b220733b531050fd22700
Edward Cessna <edward.cessna@ymedialabs.com>
2015-02-26 13:42:22 -0800

    [Apple-2255] Integrate ORK-532

---
+++ ResearchKit/ResearchKit-532-Sim/ResearchKit.framework/PrivateHeaders/ORKMediaStepViewController.h
@@ -0,0 +1,13 @@
+//
+//  ORKMediaStepViewController.h
+//  ResearchKit
+//
+//  Copyright (c) 2013-2014 Apple Inc. All rights reserved.
+//
+
+#import <ResearchKit/ResearchKit.h>
+
+ORK_CLASS_AVAILABLE
+@interface ORKMediaStepViewController : ORKStepViewController
+
+@end
