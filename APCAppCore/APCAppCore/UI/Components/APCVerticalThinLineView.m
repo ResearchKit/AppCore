e01ebf8dc8f2612ea90d35f38cb4db4ab70ebdad
Justin Warmkessel <justin.w@ymedialabs.com>
2015-02-27 15:13:48 -0800

    Adding custom UIViews to create thin lines both vertically and horizontally.

---
+++ APCAppCore/APCAppCore/UI/Components/APCVerticalThinLineView.m
@@ -0,0 +1,22 @@
+//
+//  APCVerticalThinLineView.m
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCVerticalThinLineView.h"
+
+@implementation APCVerticalThinLineView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    CALayer *TopBorder = [CALayer layer];
+    TopBorder.frame = CGRectMake(0.0f, 0.0f, 0.2f, self.frame.size.width);
+    TopBorder.backgroundColor = [UIColor colorWithRed:191/255.0 green:191/255.0 blue:191/255.0 alpha:1].CGColor;
+    [self.layer addSublayer:TopBorder];
+}
+
+@end
