af6369a8d2d4dbcaeb24e645917e365661054029
David Barnard <david.barnard@ymedialabs.com>
2015-02-27 12:28:20 -0500

    Might have commented out too much.

--- APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpMedicalInfoViewController.m
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpMedicalInfoViewController.m
@@ -158,10 +158,8 @@
                     field.textAlignnment = NSTextAlignmentRight;
                     field.pickerData = [APCUser heights];
 
-//					NSInteger defaultIndexOfMyHeightInFeet = 5;
-//					NSInteger defaultIndexOfMyHeightInInches = 0;
-//					NSInteger indexOfMyHeightInFeet = defaultIndexOfMyHeightInFeet;
-//					NSInteger indexOfMyHeightInInches = defaultIndexOfMyHeightInInches;
+					NSInteger indexOfMyHeightInFeet = 0;
+                    NSInteger indexOfMyHeightInInches = 0;
 
                     if (self.user.height) {
                         double heightInInches = round([APCUser heightInInches:self.user.height]);
@@ -172,21 +170,13 @@
 						NSArray *allPossibleHeightsInFeet = field.pickerData [0];
 						NSArray *allPossibleHeightsInInches = field.pickerData [1];
 
-//						indexOfMyHeightInFeet = [allPossibleHeightsInFeet indexOfObject: feet];
-//						indexOfMyHeightInInches = [allPossibleHeightsInInches indexOfObject: inches];
-//
-//						if (indexOfMyHeightInFeet == NSNotFound)
-//						{
-//							indexOfMyHeightInFeet = defaultIndexOfMyHeightInFeet;
-//						}
-//
-//						if (indexOfMyHeightInInches == NSNotFound)
-//						{
-//							indexOfMyHeightInInches = defaultIndexOfMyHeightInInches;
-//						}
+						indexOfMyHeightInFeet = [allPossibleHeightsInFeet indexOfObject: feet];
+						indexOfMyHeightInInches = [allPossibleHeightsInInches indexOfObject: inches];
                     }
 
-					field.selectedRowIndices = @[ @(indexOfMyHeightInFeet), @(indexOfMyHeightInInches) ];
+                    if (indexOfMyHeightInFeet && indexOfMyHeightInInches) {
+                        field.selectedRowIndices = @[ @(indexOfMyHeightInFeet), @(indexOfMyHeightInInches) ];
+                    }
 
                     APCTableViewRow *row = [APCTableViewRow new];
                     row.item = field;
