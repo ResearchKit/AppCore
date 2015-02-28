a8dd5afb5b9d5cde5bad154c9fc8a4d1f95d7a72
Justin Warmkessel <justin.w@ymedialabs.com>
2015-02-27 16:00:36 -0800

    Setting the background color to clear color for those that use the thin line views.

--- APCAppCore/APCAppCore/UI/Components/APCHorizontalThinLineView.m
+++ APCAppCore/APCAppCore/UI/Components/APCHorizontalThinLineView.m
@@ -14,6 +14,9 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
+    
+    [self setBackgroundColor:[UIColor clearColor]];
+    
     CALayer *TopBorder = [CALayer layer];
     TopBorder.frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, 0.2f);
     TopBorder.backgroundColor = [UIColor colorWithRed:191/255.0 green:191/255.0 blue:191/255.0 alpha:1].CGColor;
--- APCAppCore/APCAppCore/UI/Components/APCVerticalThinLineView.m
+++ APCAppCore/APCAppCore/UI/Components/APCVerticalThinLineView.m
@@ -13,6 +13,9 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
+    
+    [self setBackgroundColor:[UIColor clearColor]];
+    
     CALayer *TopBorder = [CALayer layer];
     TopBorder.frame = CGRectMake(0.0f, 0.0f, 0.2f, self.frame.size.width);
     TopBorder.backgroundColor = [UIColor colorWithRed:191/255.0 green:191/255.0 blue:191/255.0 alpha:1].CGColor;
