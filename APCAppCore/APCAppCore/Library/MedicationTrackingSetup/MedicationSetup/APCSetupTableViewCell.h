ef47fd2d4818af3eab08bcbeaa6e862a79012220
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-03-01 09:21:36 -0800

    [APPLE-1967] Refactoring the way we deal with required and optional Prescription Attributes

--- APCAppCore/APCAppCore/Library/MedicationTrackingSetup/MedicationSetup/APCSetupTableViewCell.h
+++ APCAppCore/APCAppCore/Library/MedicationTrackingSetup/MedicationSetup/APCSetupTableViewCell.h
@@ -10,6 +10,7 @@
 @interface APCSetupTableViewCell : UITableViewCell
 
 @property  (nonatomic, weak)  IBOutlet  UILabel  *topicLabel;
+@property  (nonatomic, weak)  IBOutlet  UILabel  *extraLabel;
 @property  (nonatomic, weak)  IBOutlet  UILabel  *addTopicLabel;
 @property  (nonatomic, weak)  IBOutlet  UIView   *colorSwatch;
 
