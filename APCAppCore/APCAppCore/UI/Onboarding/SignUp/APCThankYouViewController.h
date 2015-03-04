195741ef84b116283656eeaf310ca0d542eec667
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-03-03 21:25:54 -0800

    Adding All Set VC.

---
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCThankYouViewController.h
@@ -0,0 +1,13 @@
+//
+//  APCThankYouViewController.h
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "APCSignUpProgressing.h"
+
+@interface APCThankYouViewController : UIViewController <APCSignUpProgressing>
+
+@end
---
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCThankYouViewController.m
@@ -0,0 +1,66 @@
+//
+//  APCThankYouViewController.m
+//  APCAppCore
+//
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import "APCThankYouViewController.h"
+#import "APCAppCore.h"
+
+@interface APCThankYouViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *appName;
+
+@end
+
+@implementation APCThankYouViewController
+
+@synthesize stepProgressBar;
+
+@synthesize user = _user;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.hidesBackButton = YES;
+    
+    self.appName.text = [APCUtilities appName];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (APCOnboarding *)onboarding
+{
+    return ((APCAppDelegate *)[UIApplication sharedApplication].delegate).onboarding;
+}
+
+- (IBAction)next:(APCButton *) __unused sender {
+    [self finishOnboarding];
+}
+
+- (void)finishOnboarding
+{
+    if ([self onboarding].taskType == kAPCOnboardingTaskTypeSignIn) {
+        // We are posting this notification after .4 seconds delay, because we need to display the progress bar completion animation
+        [self performSelector:@selector(setUserSignedIn) withObject:nil afterDelay:0.4];
+    } else{
+        [self performSelector:@selector(setUserSignedUp) withObject:nil afterDelay:0.4];
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) setUserSignedUp
+{
+    self.user.signedUp = YES;
+}
+
+- (void)setUserSignedIn
+{
+    self.user.signedIn = YES;
+}
+
+@end
