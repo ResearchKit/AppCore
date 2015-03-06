089e49985299a8b8f62c8649adc88be0934ac743
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-03-06 11:24:41 -0800

    Adding the 'dot' at the end of the tooltip text.

--- APCAppCore/APCAppCore/UI/ViewControllers/Activity Tracking/APCActivityTrackingStepViewController.m
+++ APCAppCore/APCAppCore/UI/ViewControllers/Activity Tracking/APCActivityTrackingStepViewController.m
@@ -13,7 +13,7 @@ static NSInteger const kYesterdaySegmentIndex    = 0;
 static NSInteger const kTodaySegmentIndex        = 1;
 static NSInteger const kWeekSegmentIndex         = 2;
 
-static NSString   *kLearnMoreString = @"The circle shows estimates of the proportion of time you have been spending in different levels of activity, based on sensor data from your phone or wearable device. It also estimates your accumulated “active minutes,” which combines moderate and vigorous activities, and daily steps. This is intended to be informational, as accurate assessment of every type of activity from sensors is an ongoing area of research and development. Your data can help us refine these estimates and better understand the relationship between activity and heart health";
+static NSString   *kLearnMoreString = @"The circle shows estimates of the proportion of time you have been spending in different levels of activity, based on sensor data from your phone or wearable device. It also estimates your accumulated “active minutes,” which combines moderate and vigorous activities, and daily steps. This is intended to be informational, as accurate assessment of every type of activity from sensors is an ongoing area of research and development. Your data can help us refine these estimates and better understand the relationship between activity and heart health.";
 
 static NSInteger const kSmallerFontSize = 16;
 static NSInteger const kRegularFontSize = 17;
