af7c673ee3366fd008679c48d560ec29ae9e7b32
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-03-06 12:35:13 -0800

    [APPLE-2556] Added Warning That Fix will be Re-Factored into Application Level in Future

--- APCAppCore/APCAppCore/UI/TabBarControllers/Activities/APCActivitiesViewController.m
+++ APCAppCore/APCAppCore/UI/TabBarControllers/Activities/APCActivitiesViewController.m
@@ -154,6 +154,7 @@ static CGFloat kTableViewSectionHeaderHeight = 77;
     else if ([task isKindOfClass:[APCScheduledTask class]])
     {        
         cell.titleLabel.text = scheduledTask.task.taskTitle;
+#warning This is a Temporary Fix and Will be Re-Factored into Application-Level
         if ([scheduledTask.task.taskID isEqualToString:medicationTrackerTaskId] == NO) {
             cell.confirmationView.completed = scheduledTask.completed.boolValue;
         }
