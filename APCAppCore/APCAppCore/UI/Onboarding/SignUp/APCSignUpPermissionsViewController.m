0e6550b5307e3a9deab49892d3f90abfd1584c08
Chris Wardell <chris.wardell@Chris-Wardells-MacBook-Pro.local>
2015-03-31 14:14:16 -0700

    [APPLE-2788] Add observer for UIApplicationWillEnterForegroundNotification

--- APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpPermissionsViewController.m
+++ APCAppCore/APCAppCore/UI/Onboarding/SignUp/APCSignUpPermissionsViewController.m
@@ -106,6 +106,7 @@ static CGFloat const kTableViewRowHeight                 = 200.0f;
     [super viewWillAppear:animated];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActive:) name:UIApplicationWillEnterForegroundNotification object:nil];
 }
 
 -(void)dealloc{
