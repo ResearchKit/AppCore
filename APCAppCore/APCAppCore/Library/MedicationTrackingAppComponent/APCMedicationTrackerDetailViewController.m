420b1695278b5d18dcd7087a88956d7f59cc0595
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-03-31 10:04:08 -0700

    APPLE-2707 - Text truncation in Medication Details edit screen
    Modified XIB Documents for two-line labels for medication names
    
    Truncated long medication names at ' (' boundaries
    
    Signed-off-by: Henry McGilton <henry.mcgilton@ymedialabs.com>

--- APCAppCore/APCAppCore/Library/MedicationTrackingAppComponent/APCMedicationTrackerDetailViewController.m
+++ APCAppCore/APCAppCore/Library/MedicationTrackingAppComponent/APCMedicationTrackerDetailViewController.m
@@ -1,10 +1,36 @@
-//
-//  APCMedicationTrackerDetailViewController.m
-//  APCAppCore
-//
-//  Copyright (c) 2015 Apple, Inc. All rights reserved.
-//
-
+// 
+//  APCMedicationTrackerDetailViewController.m 
+//  APCAppCore 
+// 
+// Copyright (c) 2015, Apple Inc. All rights reserved. 
+// 
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+// 
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+// 
+// 2.  Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the following disclaimer in the documentation and/or 
+// other materials provided with the distribution. 
+// 
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors 
+// may be used to endorse or promote products derived from this software without 
+// specific prior written permission. No license is granted to the trademarks of 
+// the copyright holders even if such marks are included in this software. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE 
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+// 
+ 
 #import "APCMedicationTrackerDetailViewController.h"
 #import "APCSetupTableViewCell.h"
 #import "APCLozengeButton.h"
@@ -67,15 +93,35 @@ static  CGFloat    kAPCMedicationRowHeight       = 64.0;
     return  numberOfRows;
 }
 
+- (NSString *)extractMedicationNamePrefix:(NSString *)medicationName
+{
+    NSRange  range = [medicationName rangeOfString:@" ("];
+    NSString  *firstString = nil;
+    NSString  *secondString = nil;
+    if (range.location == NSNotFound) {
+        firstString = medicationName;
+    } else {
+        firstString = [medicationName substringToIndex:range.location];
+        secondString = [medicationName substringFromIndex:(range.location + 1)];
+    }
+    return  firstString;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *) __unused indexPath
 {
     APCMedicationDetailsTableViewCell  *cell = [tableView dequeueReusableCellWithIdentifier:kMedicationDetailsName];
+    cell.contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
     cell.colorSwatch.backgroundColor = self.lozenge.prescription.color.UIColor;
-    cell.medicationName.text = self.lozenge.prescription.medication.name;
+    
+    cell.medicationName.text = [self extractMedicationNamePrefix:self.lozenge.prescription.medication.name];
+    
     NSString  *doseNumberString = [NSString stringWithFormat:@"Dose %ld", (indexPath.row + 1)];
     cell.doseNumber.text = doseNumberString;
-    NSString  *doseAmountString = [NSString stringWithFormat:@"(%@)", self.lozenge.prescription.dosage.name];
+    
+    NSString  *doseAmountString = [NSString stringWithFormat:@"%@", self.lozenge.prescription.dosage.name];
     cell.doseAmount.text = doseAmountString;
     if (self.numberOfTickMarksToSet > 0) {
         cell.confirmer.completed = YES;
