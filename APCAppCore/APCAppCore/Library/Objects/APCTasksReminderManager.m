c3b069c4824e18864f9d416af5d6274c9dce8851
Chris Wardell <chris.wardell@Chris-Wardells-MacBook-Pro.local>
2015-05-01 09:01:34 -0700

    [APPLE-2957] Only default reminders to on if notification permission given

--- APCAppCore/APCAppCore/Library/Objects/APCTasksReminderManager.m
+++ APCAppCore/APCAppCore/Library/Objects/APCTasksReminderManager.m
@@ -306,9 +306,11 @@ NSString * const kTaskReminderDelayMessage = @"Remind me in 1 hour";
     NSNumber * number = [[NSUserDefaults standardUserDefaults] objectForKey:kTasksReminderDefaultsOnOffKey];
     //Setting up defaults using initialization options
     if (number == nil) {
-        //default to on
-        [[NSUserDefaults standardUserDefaults] setObject:@YES forKey:kTasksReminderDefaultsOnOffKey];
-        [[NSUserDefaults standardUserDefaults] synchronize];
+        //default to on if user has given Notification permissions
+        if ([[UIApplication sharedApplication] currentUserNotificationSettings].types != UIUserNotificationTypeNone){
+            [[NSUserDefaults standardUserDefaults] setObject:@YES forKey:kTasksReminderDefaultsOnOffKey];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
     }
     
     return [number boolValue];
