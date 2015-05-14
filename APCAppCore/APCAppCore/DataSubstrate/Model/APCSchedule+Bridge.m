987f20c15e76d556f3ce0fd0b8d91787ff199b9b
Farhan Ahmed <farhan.ahmed@ymedialabs.com>
2015-05-14 11:26:20 -0700

    Including missing header files to make AppCore compile.
    Also removing references to Demographics uploader.

--- APCAppCore/APCAppCore/DataSubstrate/Model/APCSchedule+Bridge.m
+++ APCAppCore/APCAppCore/DataSubstrate/Model/APCSchedule+Bridge.m
@@ -33,12 +33,11 @@
  
 #import "APCSchedule+Bridge.h"
 #import "APCAppDelegate.h"
-#import "APCTask.h"
+#import "APCTask+AddOn.h"
 #import "APCLog.h"
-
 #import "NSManagedObject+APCHelper.h"
+#import "SBBGuidCreatedOnVersionHolder+APCAdditions.h"
 
-#import <BridgeSDK/BridgeSDK.h>
 
 NSString *const kSurveyTaskViewController = @"APCGenericSurveyTaskViewController";
 
