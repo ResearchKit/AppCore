1bdc86cec6e77e1bb1dac275877e4d34ec9ac7d4
Justin Warmkessel <justin.w@ymedialabs.com>
2015-07-07 13:45:34 -0700

    Reset method name to use stable version of Researchkit

--- APCAppCore/APCAppCore/DataSubstrate/Model/APCSmartSurveyTask.m
+++ APCAppCore/APCAppCore/DataSubstrate/Model/APCSmartSurveyTask.m
@@ -549,7 +549,7 @@ static APCDummyObject * _dummyObject;
     NSMutableArray * options = [NSMutableArray array];
     [localConstraints.enumeration enumerateObjectsUsingBlock:^(SBBSurveyQuestionOption* option, NSUInteger __unused idx, BOOL * __unused stop) {
         NSString * detailText = option.detail.length > 0 ? option.detail : nil;
-        ORKTextChoice * choice = [ORKTextChoice choiceWithText:option.label detailText:detailText value:option.value exclusive:NO];
+        ORKTextChoice * choice = [ORKTextChoice choiceWithText:option.label detailText:detailText value:option.value];
         [options addObject: choice];
     }];
     if (localConstraints.allowOtherValue) {
